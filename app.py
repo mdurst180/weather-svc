0ed6fbccb4dc2c110a819a83e42d74b9a6b8375c
Mike Durst <mdurst180@gmail.com>
2021-11-30 15:03:47 -0500

    add weather param

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def get_weather():
   # returns the current day's forecast temperature (int)
   print(weather.current.temperature)
 
-  return jsonify(json.dumps(weather))
+  return jsonify(json.dumps(weather.current))
 
 @app.route('/')
 def hello():
