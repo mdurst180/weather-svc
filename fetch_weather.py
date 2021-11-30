62591a4beeb0a94d4fb8c1d7aff295af9cca3503
Mike Durst <mdurst180@gmail.com>
2021-11-30 15:26:52 -0500

    add weather fetch job

--- fetch_weather.py
+++ fetch_weather.py
@@ -1,6 +1,8 @@
 import asyncio
 
 import json
+
+from flask.json import jsonify
 import weather
 
 print("Hello!")
@@ -8,3 +10,9 @@ print("Hello!")
 city = "Philadelphia"
 loop = asyncio.get_event_loop()
 current_weather = loop.run_until_complete(weather.getweather(city))
+
+print(jsonify({
+    'city': city,
+    'current_temp': current_weather.current.temperature,
+    'feels_like': current_weather.current.feels_like,
+  }))
