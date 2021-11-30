4a171aa655f32629fa7685a25b35e51fe39b0795
Mike Durst <mdurst180@gmail.com>
2021-11-30 15:53:20 -0500

    add weather fetch job

--- fetch_weather.py
+++ fetch_weather.py
@@ -1,8 +1,13 @@
+
+import os
+
 import asyncio
 
 import json
 
 import weather
+
+import send_txt
 
 print("Hello!")
 
@@ -15,3 +20,17 @@ print({
     'current_temp': current_weather.current.temperature,
     'feels_like': current_weather.current.feels_like,
   })
+
+
+
+
+_num = "2156208067"
+_carrier = "at&t"
+_email = "mdurst180@gmail.com"
+_pword = os.environ['MAIL']
+_msg = "Dummy msg"
+_subj = "Dummy subj"
+coro = send_txt.send_txt(_num, _carrier, _email, _pword, _msg, _subj)
+# _nums = {"999999999", "000000000"}
+# coro = send_txts(_nums, _carrier, _email, _pword, _msg, _subj)
+asyncio.run(coro)
