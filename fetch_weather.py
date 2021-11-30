5920d772a381217090c49b9a02c183d74dc7bf4b
Mike Durst <mdurst180@gmail.com>
2021-11-30 15:25:32 -0500

    add weather fetch job

--- fetch_weather.py
+++ fetch_weather.py
@@ -1,3 +1,10 @@
+import asyncio
 
+import json
+import weather
 
 print("Hello!")
+
+city = "Philadelphia"
+loop = asyncio.get_event_loop()
+current_weather = loop.run_until_complete(weather.getweather(city))
